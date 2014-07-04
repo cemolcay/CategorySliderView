33d03b91d2f585eaaa1b9258bd748e344fd488ee
cemolcay <ccemolcay@gmail.com>
2014-07-04 13:24:50 +0300

    Initial Commit

---
+++ CategorySliderView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CategorySliderView
+//
+//  Created by Cem Olcay on 04/07/14.
+//  Copyright (c) 2014 questamobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CategorySliderViewTests/CategorySliderViewTests.m
@@ -0,0 +1,34 @@
+//
+//  CategorySliderViewTests.m
+//  CategorySliderViewTests
+//
+//  Created by Cem Olcay on 04/07/14.
+//  Copyright (c) 2014 questamobile. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CategorySliderViewTests : XCTestCase
+
+@end
+
+@implementation CategorySliderViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
